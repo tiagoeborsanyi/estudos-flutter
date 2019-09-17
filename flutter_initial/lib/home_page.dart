46f7213507119cd288cd693daa6aeb7f464e4ecf
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-09-17 10:45:27 -0300

    estudando margem e padding

--- flutter_initial/lib/home_page.dart
+++ flutter_initial/lib/home_page.dart
@@ -16,12 +16,55 @@ class HomePage extends StatelessWidget {
 
   _body() {
     return Container(
-//      margin: EdgeInsets.only(left: 10, top: 10),
       color: Colors.white,
-      child: Center(
-        child: _img(),
-      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _text(),
+          _pageView(),
+          _buttons()
+        ],
+      )
     );
+  }
+
+  Container _pageView() {
+    return Container(
+          margin: EdgeInsets.only(top: 20, bottom: 20),
+          height: 300,
+          child: PageView(
+            children: <Widget>[
+              _img("assets/images/dog1.png"),
+              _img("assets/images/dog2.png"),
+              _img("assets/images/dog3.png"),
+              _img("assets/images/dog4.png"),
+              _img("assets/images/dog5.png"),
+            ],
+          ),
+        );
+  }
+
+ _buttons() {
+    return Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _button("List View"),
+                _button("Page 2"),
+                _button("Page 3"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _button("Snack"),
+                _button("Dialog"),
+                _button("Toast"),
+              ],
+            )
+          ],
+        );
   }
 
   _text() {
@@ -39,7 +82,31 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _img() {
-    return Image.asset("assets/images/dog2.png");
+  _button(String text) {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+      onPressed: () => _onClickOk()
+    );
+  }
+
+  void _onClickOk() {
+    print("OK!");
+  }
+
+  _img(String img) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Image.asset(
+        img,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
