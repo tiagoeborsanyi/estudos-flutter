6f3cb496283388c03df85fce440fbbc35f5068ce
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-07 23:32:44 -0300

    criada a estrutura inicial do projeto

---
+++ carros/carros/lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+
+    );
+  }
+}
