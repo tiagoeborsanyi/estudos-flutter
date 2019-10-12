e135eabffcd8170f26a5a7649cc7c0b3ed3d2e69
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-12 10:16:42 -0300

    stream bloc form login - carros

--- carros/carros/lib/pages/login/login_page.dart
+++ carros/carros/lib/pages/login/login_page.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:carros/pages/api_response.dart';
 import 'package:carros/pages/carro/home_page.dart';
-import 'package:carros/pages/login/login_api.dart';
+import 'package:carros/pages/login/login_bloc.dart';
 import 'package:carros/pages/login/usuario.dart';
 import 'package:carros/utils/alert.dart';
 import 'package:carros/utils/nav.dart';
@@ -18,7 +18,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
 
-  final _streamController = StreamController<bool>();
+  final _bloc = LoginBloc();
 
   final _tLogin = TextEditingController();
 
@@ -86,7 +86,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 20,
             ),
             StreamBuilder<bool>(
-              stream: _streamController.stream,
+              stream: _bloc.stream,
               builder: (context, snapshot) {
                 return AppButton(
                   "Login",
@@ -110,15 +110,12 @@ class _LoginPageState extends State<LoginPage> {
     String login = _tLogin.text;
     String senha = _tSenha.text;
 
-    _streamController.add(true);
-
-    ApiResponse response = await LoginApi.login(login, senha);
+    ApiResponse response = await _bloc.login(login, senha);
     if (response.ok) {
       push(context, HomePage(), replace: true);
     } else {
       alert(context, response.msg);
     }
-    _streamController.add(false);
   }
 
   String _validateLogin(String value) {
@@ -139,6 +136,6 @@ class _LoginPageState extends State<LoginPage> {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    _streamController.close();
+    _bloc.dispose();
   }
 }
